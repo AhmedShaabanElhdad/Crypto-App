17427c28721642b4e3283e4eada45ced714bb30c
Ahmed Shaaban <ahmed.shaaban.elhdah@gmail.com>
2024-04-05 12:16:30 +0300

    Create Home Header

--- Crypto App/Core/Home/Views/HomeView.swift
+++ Crypto App/Core/Home/Views/HomeView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showProfile: Bool = false
+    
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
             VStack {
-                Text("Header")
+                homeHeader
                 Spacer(minLength: 0)
+            }.padding()
+        }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showProfile ? "plus" : "info").animation(.none)
+                .background(
+                    CircleButtonWithAnimationView(animate: $showProfile)
+                )
+            Spacer()
+            Text(showProfile ? "protofolio" : "Live Price").animation(.none)
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.theme.accent)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showProfile ? 180 : 0)).onTapGesture {
+                withAnimation(.spring){
+                    showProfile.toggle()
+                }
             }
         }
     }
@@ -22,5 +48,4 @@ struct HomeView: View {
 
 #Preview {
     HomeView()
-        .colorScheme(.dark)
 }
