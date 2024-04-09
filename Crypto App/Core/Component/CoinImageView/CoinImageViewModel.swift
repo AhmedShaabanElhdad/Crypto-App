9e667e2d1c34db6114ac69c87b5364d497b83a81
Ahmed Shaaban <ahmed.shaaban.elhdah@gmail.com>
2024-04-09 15:48:34 +0300

    Create Cpon image viewmodel
    - Create ImageView
    - Create Viewmodel for coin
    - use combine for call the internet

--- Crypto App/Core/Component/CoinImageView/CoinImageViewModel.swift
+++ Crypto App/Core/Component/CoinImageView/CoinImageViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by ahmed shaban on 08.04.2024.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+
+
+class CoinImageViewModel : ObservableObject{
+    @Published var image: UIImage? = nil
+    @Published var loading: Bool = false
+    
+    private let dataService: CoinImageService
+    private let coin: CoinModel
+    private var cancellables = Set<AnyCancellable>()
+    
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(urlString: coin.image)
+        self.loading = true
+        getImage()
+    }
+    
+    func getImage(){
+        dataService.$image
+            .sink { [weak self] (_) in
+                self?.loading = false
+            } receiveValue: { [weak self] (returnedImageData) in
+                guard let image = returnedImageData else { return }
+                self?.image = UIImage(data: image)
+            }.store(in: &cancellables)
+    
+
+    }
+}
