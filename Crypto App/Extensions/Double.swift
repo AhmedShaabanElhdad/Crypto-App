e849c0304916d1fc83fe2c7b3f679e7c18d5b176
Ahmed Shaaban <ahmed.shaaban.elhdah@gmail.com>
2024-04-23 18:06:25 +0300

    Add breviation for number in statstic viewm

--- Crypto App/Extensions/Double.swift
+++ Crypto App/Extensions/Double.swift
@@ -33,4 +33,32 @@ extension Double {
     func asNumberStringWithPrecentage() -> String {
         return asNumberString() + "%"
     }
+    
+    func formattedWithAbbreviations() -> String {
+        let num = abs(Double(self))
+        let sign = (self < 0) ? "-" : ""
+        switch num {
+        case 1_000_000_000_000... :
+            let formatted = num / 1_000_000_000_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)Tr"
+            
+        case 1_000_000_000... :
+            let formatted = num / 1_000_000_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)Bn"
+            
+        case 1_000_000... :
+            let formatted = num / 1_000_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)M"
+            
+        case 1_000... :
+            let formatted = num / 1_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)K"
+        default:
+            return "\(sign)\(self)"
+        }
+    }
 }
