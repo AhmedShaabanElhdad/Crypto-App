6d908b3eef6e010e232fdeb0d833f6bf3566b8fd
Ahmed Shaban  Elhdad <ahmed.shaaban.elhdah@gmail.com>
2024-05-03 09:36:43 +0300

    Create Portofolio view (#27)
    - Create Portofolio View
    - Create Coin Input View

--- Crypto App/Core/Home/Views/ProtofolioView.swift
+++ Crypto App/Core/Home/Views/ProtofolioView.swift
@@ -11,33 +11,18 @@ struct ProtofolioView: View {
     
     @EnvironmentObject var viewmodel: HomeViewModel
     @State var selectedCoin:CoinModel? = nil
+    @State private var quantitytxt = ""
+    @State private var showCheckMark: Bool = false
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 10) {
                     SearchView(searchTextView: $viewmodel.searchText)
-                    Spacer(minLength: 10)
-                    ScrollView(.horizontal, showsIndicators: false,content: {
-                        LazyHStack(spacing: 10){
-                            ForEach(viewmodel.allCoins) { coin in
-                                CoinHorizontalView(coinModel: coin)
-                                    .onTapGesture {
-                                        withAnimation(.easeIn){
-                                            selectedCoin = coin
-                                        }
-                                    }
-                                    .frame(width: 75)
-                                    .padding(4)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .strokeBorder(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear ,lineWidth: 1)
-                                    )
-                            }
-                        }
-                        .padding(.vertical,4)
-                        .padding(.leading)
-                    })
+                    coinLogoList
+                    if selectedCoin != nil {
+                        portofolioInputSection
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -45,8 +30,104 @@ struct ProtofolioView: View {
                 ToolbarItem(placement: .navigationBarLeading){
                     XMarkButtom()
                 }
+                
+                ToolbarItem(placement: .navigationBarTrailing){
+                    HStack(spacing: 0){
+                        Image(systemName: "checkmark")
+                            .opacity(showCheckMark ? 1.0 : 0)
+                        
+                        Button(action: {
+                            saveButtonPressed()
+                        }, label: {
+                            Text("save".uppercased())
+                        }).opacity( (selectedCoin != nil && selectedCoin?.currentHolding != Double(quantitytxt)) ? 1.0 : 0)
+                    }.font(.headline)
+                }
             })
         }
+    }
+}
+
+extension ProtofolioView{
+    private var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false,content: {
+            LazyHStack(spacing: 10){
+                ForEach(viewmodel.allCoins) { coin in
+                    CoinHorizontalView(coinModel: coin)
+                        .onTapGesture {
+                            withAnimation(.easeIn){
+                                selectedCoin = coin
+                            }
+                        }
+                        .frame(width: 75)
+                        .padding(4)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .strokeBorder(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear ,lineWidth: 1)
+                        )
+                }
+            }
+            .frame(height: 120)
+            .padding(.leading)
+        })
+    }
+    
+    private var portofolioInputSection: some View {
+        VStack(spacing: 10){
+            HStack{
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? "" ):")
+                Spacer()
+                Text("\(selectedCoin?.currentPrice.asCurrency6Digit() ?? "" )")
+            }
+            Divider()
+            
+            HStack{
+                Text("Portofolio Amount:")
+                Spacer()
+                TextField("Ex: 1.4",text: $quantitytxt)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+            }
+            Divider()
+            HStack{
+                Text("Current Value :")
+                Spacer()
+                Text(getValue().asNumberString())
+            }
+        }
+        .animation(.none)
+        .font(.headline)
+        .padding()
+    }
+    
+    private func getValue() -> Double{
+        if let value = Double(quantitytxt) {
+            return value * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
+    }
+    
+    private func saveButtonPressed(){
+        guard let coin = selectedCoin else { return }
+        
+        withAnimation(.easeIn){
+            removeSelection()
+            showCheckMark = true
+        }
+        
+        UIApplication.shared.endEditing()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0 ){
+            withAnimation(.easeOut){
+                showCheckMark = false
+            }
+        }
+    }
+    
+    private func removeSelection(){
+        selectedCoin = nil
+        viewmodel.searchText = ""
+        quantitytxt = ""
     }
 }
 
